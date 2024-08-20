aec0b455c8579d8f79e43544cc2d61fe8e928006
Keith W. Boone <36998841+keithboone@users.noreply.github.com>
2024-08-20 14:30:04 -0400

    Restrict logging for non-test messages. (#9)
    * Restrict logging for non-test messages.
    * Fix .classpath
    * Upgrade spring-framework to 6.1.12 for CVE-2024-38809
    * Updating version for Release

--- src/main/java/gov/cdc/izgateway/soap/fault/SecurityFault.java
+++ src/main/java/gov/cdc/izgateway/soap/fault/SecurityFault.java
@@ -4,6 +4,12 @@ import gov.cdc.izgateway.logging.info.EndPointInfo;
 import gov.cdc.izgateway.model.RetryStrategy;
 import lombok.Getter;
 
+/**
+ * SecurityFault is used to report issues regarding the security of connections or messaging.
+ * 
+ * @author Audacious Inquiry
+ *
+ */
 public class SecurityFault extends Fault {
 	private static final long serialVersionUID = 1L;
 	@Getter 
@@ -29,15 +35,33 @@ public class SecurityFault extends Fault {
         this.endpoint = endpoint;
     }
 
+    /**
+     * Report a general security issue not elsewhere specified below.
+     * @param summary	The summary message, should be the same in all cases where the issue is the same
+     * @param detail	The detailed part of the message, provides specifics for each case
+     * @param cause		The cause of the exception
+     * @return	A new security fault ready to be thrown
+     */
     public static SecurityFault generalSecurity(String summary, String detail, Throwable cause) {
     	return new SecurityFault(MESSAGE_TEMPLATES[0].copy().setSummary(summary, detail), cause, null);
     }
 
 
+    /**
+     * Report a detected potential attack from a message sender or from a recipient's response
+     * @param detail	More details about what was found
+     * @param endpoint	The endpoint that was the source of the suspected attack
+     * @return	A new security fault ready to be thrown
+     */
     public static SecurityFault sourceAttack(String detail, EndPointInfo endpoint) {
     	return new SecurityFault(MESSAGE_TEMPLATES[1].copy().setDetail(detail), null, endpoint);
 	}
     
+    /**
+     * Report an attempt to access information by a blacklisted user
+     * @param endpoint	The endpoint that is attempting the access
+     * @return	A new security fault ready to be thrown
+     */
     public static SecurityFault userBlacklisted(EndPointInfo endpoint) {
         return new SecurityFault(MESSAGE_TEMPLATES[2].copy().setDetail(endpoint.getCommonName()), null, endpoint);
 	}

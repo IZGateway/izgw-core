5fec080348b8d33fe6716096ac32f088cd5a8c8c
Paul Cahill <pcahill@ainq.com>
2025-02-19 14:23:58 -0500

    Refactoring to align with single responsibility principle.

--- src/main/java/gov/cdc/izgateway/security/principal/CertificatePrincipalProviderImpl.java
+++ src/main/java/gov/cdc/izgateway/security/principal/CertificatePrincipalProviderImpl.java
@@ -1,59 +1,69 @@
 package gov.cdc.izgateway.security.principal;
 
 import gov.cdc.izgateway.principal.provider.CertificatePrincipalProvider;
-import gov.cdc.izgateway.security.CertificatePrincipal;
-import gov.cdc.izgateway.security.ClientTlsSupport;
-import gov.cdc.izgateway.security.IzgPrincipal;
+import gov.cdc.izgateway.security.*;
 import gov.cdc.izgateway.utils.X500Utils;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.Globals;
 import org.apache.commons.lang3.StringUtils;
-import org.bouncycastle.util.io.pem.PemObject;
-import org.bouncycastle.util.io.pem.PemReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.net.ssl.X509TrustManager;
 import javax.security.auth.x500.X500Principal;
-import java.io.ByteArrayInputStream;
-import java.io.StringReader;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.security.cert.*;
 import java.util.Map;
 
 /**
- * A Principal Provider that uses certificates to identify the principal
- * by common name and organization.
- *
- * @author Audacious Inquiry
- *
+ * This class provides a principal from a certificate
  */
 @Slf4j
 @Component
 public class CertificatePrincipalProviderImpl implements CertificatePrincipalProvider {
-    // The header key that contains the certificate (populated by the ALB )
     @Value("${client.ssl.certificate-header:}")
     private String certHeaderKey;
 
-    private final ClientTlsSupport clientTlsSupport;
+    private final CertificateValidator validator;
 
     @Autowired
     public CertificatePrincipalProviderImpl(ClientTlsSupport clientTlsSupport) {
-        this.clientTlsSupport = clientTlsSupport;
+        this.validator = new CertificateValidator((X509TrustManager) clientTlsSupport.getTrustManagers()[0]);
     }
 
     @Override
     public IzgPrincipal createPrincipalFromCertificate(HttpServletRequest request) {
         X509Certificate cert = getCertificate(request);
+        return cert != null ? createPrincipalFromCertificate(cert) : null;
+    }
 
-        if (cert == null) {
+    /**
+     * Gets the certificate from two possible places.  If a certificate is present in the request attribute,
+     * it is returned. If not, the certificate is extracted from the header.  Null is returned if no certificate
+     * is found or if the certificate is invalid.
+     * @param request	The request
+     * @return	The certificate
+     */
+    private X509Certificate getCertificate(HttpServletRequest request) {
+        X509Certificate cert = getCertificateFromAttribute(request);
+        if (cert != null) return cert;
+
+        String certHeader = request.getHeader(certHeaderKey);
+        if (StringUtils.isBlank(certHeader)) return null;
+
+        try {
+            cert = CertificateProcessor.processCertificateFromHeader(certHeader);
+            return validator.isValid(cert) ? cert : null;
+        } catch (CertificateException e) {
+            log.error("Failed to process certificate from header", e);
             return null;
         }
+    }
 
-        return createPrincipalFromCertificate(cert);
+    private X509Certificate getCertificateFromAttribute(HttpServletRequest request) {
+        X509Certificate[] certs = (X509Certificate[]) request.getAttribute(Globals.CERTIFICATES_ATTR);
+        return (certs != null && certs.length > 0) ? certs[0] : null;
     }
 
     /**
@@ -80,63 +90,5 @@ public class CertificatePrincipalProviderImpl implements CertificatePrincipalPro
         return principal;
     }
 
-    /**
-     * Returns the certificate from the request attribute if it exists, otherwise
-     * it will attempt to parse the certificate from the header (populated by the ALB).
-     * @param request The HTTPServletRequest
-     * @return The certificate or null if it cannot be found
-     */
-    private X509Certificate getCertificate(HttpServletRequest request) {
-        X509Certificate cert = null;
-
-        // Check for certificate in request attribute
-        X509Certificate[] certs = (X509Certificate[]) request.getAttribute(Globals.CERTIFICATES_ATTR);
-        if (certs != null && certs.length > 0) {
-            cert = certs[0];
-        } else {
-            // Check for certificate in header
-            String certHeader = request.getHeader(certHeaderKey);
-            if (StringUtils.isNotBlank(certHeader)) {
-                // The certificate is base64 encoded and URL encoded
-                certHeader = certHeader.replace("+", "%2B");
-                certHeader = URLDecoder.decode(certHeader, StandardCharsets.UTF_8);
-                try (PemReader pemReader = new PemReader(new StringReader(certHeader))) {
-                    PemObject pemObject = pemReader.readPemObject();
-                    byte[] decodedCert = pemObject.getContent();
-                    CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
-                    cert = (X509Certificate) certFactory.generateCertificate(new ByteArrayInputStream(decodedCert));
-                    cert.checkValidity();
-
-                    // Validate the certificate chain
-                    if (!isValidCertificateChain(cert)) {
-                        log.error("Invalid certificate chain");
-                        return null;
-                    } else {
-                        log.info("Valid certificate chain");
-                    }
-                } catch (Exception e) {
-                    log.error("Failed to parse certificate from header", e);
-                    return null;
-                }
-            }
-        }
-
-        return cert;
-    }
-
-    private boolean isValidCertificateChain(X509Certificate cert) {
-        try {
-            X509TrustManager trustManager = (X509TrustManager) clientTlsSupport.getTrustManagers()[0];
-
-            trustManager.checkClientTrusted(new X509Certificate[]{cert}, "TLS-client-auth");
-
-            return true;
-        } catch (CertificateException e) {
-            log.error("Certificate chain validation failed", e);
-            return false;
-        } catch (Exception e) {
-            log.error("Unexpected error during certificate chain validation", e);
-            return false;
-        }
-    }
 }
+
